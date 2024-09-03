7f1e4f7177e46f56c8051ab0d758eacf11cbea52
Elías Zelada <elixs.777@gmail.com>
2024-09-03 01:31:39 -0400

    added walk/run logic

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,13 +2,16 @@ class_name Player
 extends CharacterBody2D
 
 
-@export var speed = 400
+@export var speed = 200
+@export var run_speed = 400
 @export var jump_speed = 600
 @export var gravity = 1500
 @export var acceleration = 2000
 @export var attacking = false
 @export var ball_scene: PackedScene
 @export var jump_stream: AudioStream
+
+var running := false
 
 @onready var sprite_2d: Sprite2D = $Pivot/Sprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
@@ -19,6 +22,7 @@ extends CharacterBody2D
 @onready var ball_spawn: Marker2D = $Pivot/BallSpawn
 @onready var fire_cooldown: Timer = $FireCooldown
 @onready var auto_fire_timer: Timer = $AutoFireTimer
+@onready var blink_timer: Timer = $BlinkTimer
 
 
 func _ready() -> void:
@@ -26,16 +30,20 @@ func _ready() -> void:
 	animation_tree.active = true
 	hitbox.damage_dealt.connect(_on_damage_dealt)
 	auto_fire_timer.timeout.connect(fire)
+	blink_timer.timeout.connect(_on_blink_timeout)
 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	_handle_running()
 
 	var move_input = Input.get_axis("move_left", "move_right")
 	if attacking:
 		move_input = 0
-	velocity.x = move_toward(velocity.x, speed *  move_input, acceleration * delta)
+	var target_speed = run_speed if running else speed
+	velocity.x = move_toward(velocity.x, target_speed *  move_input, acceleration * delta)
 	
 	if not attacking and is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -jump_speed
@@ -63,6 +71,8 @@ func _physics_process(delta: float) -> void:
 			playback.travel("jump")
 		else:
 			playback.travel("fall")
+	
+	animation_tree["parameters/movement/run/blend_position"] = abs(velocity.x)
 
 
 func taunt():
@@ -71,10 +81,6 @@ func taunt():
 
 func take_damage(damage: int):
 	Debug.log("oh no")
-
-
-func _on_damage_dealt() -> void:
-	Debug.log("We made damage")
 
 
 func fire() -> void:
@@ -87,3 +93,18 @@ func fire() -> void:
 	get_parent().add_child(ball_inst)
 	ball_inst.global_position = ball_spawn.global_position + Vector2(randf_range(-100, 100), randf_range(-100, 100))
 	fire_cooldown.start()
+
+
+func _on_damage_dealt() -> void:
+	Debug.log("We made damage")
+
+
+func _on_blink_timeout() -> void:
+	animation_tree["parameters/movement/idle/playback"].travel("blink")
+
+
+func _handle_running():
+	if is_on_floor() and not running and Input.is_action_pressed("run"):
+		running = true
+	if Input.is_action_just_released("run"):
+		running = false
